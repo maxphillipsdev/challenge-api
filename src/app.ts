a704cef7c158d84cc94790bd4a4d69c53eaa19dc
Max Phillips <max@maxphillipsdev.com>
2021-12-14 18:26:48 +1100

    Setup middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import bodyParser from "body-parser";
 import express from "express";
+import logger from "morgan";
 
+// Create Express app
 const app = express();
+
+// Initialize middleware
+app.use(bodyParser.json());
+app.use(logger("combined"));
 
 app.get("/", (req, res) => {
   res.status(404).send({
