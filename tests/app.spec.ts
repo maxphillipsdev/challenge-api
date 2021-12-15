4b581c6be155eb9771563ff913fca9f75b1751a9
Max Phillips <max@maxphillipsdev.com>
2021-12-15 22:18:17 +1100

    Fix tests

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -2,6 +2,7 @@ import { response } from "express";
 import request, { Response } from "supertest";
 import mockRequest from "../mocks/mockRequest";
 import app from "../src/app";
+import { ReducedShow } from "../src/types";
 
 describe("GET /", () => {
   test("Should respond 404 to GET /", async () => {
@@ -18,21 +19,23 @@ describe("POST / with shows payload", () => {
 
   test("Has a 'response' key in response.", async () => {
     const response = await request(app).post("/").send(mockRequest);
-    expect(response.body).toContain("response");
+    expect(Object.keys(response.body)).toContain("response");
   });
 
   test("Matches expected structure", async () => {
     const response = await request(app).post("/").send(mockRequest);
     const shows = response.body?.response;
-    expect(shows).toHaveProperty("image");
-    expect(shows).toHaveProperty("slug");
-    expect(shows).toHaveProperty("title");
+    shows.forEach((show: ReducedShow) => {
+      expect(show).toHaveProperty("image");
+      expect(show).toHaveProperty("slug");
+      expect(show).toHaveProperty("title");
+    });
   });
 
   test("Empty response on empty body", async () => {
     const response = await request(app).post("/").send({ payload: [] });
     expect(response.body).toStrictEqual({
-      payload: [],
+      response: [],
     });
   });
 
@@ -40,6 +43,6 @@ describe("POST / with shows payload", () => {
     const response = await request(app)
       .post("/")
       .send({ badPayload: "Please hire me thanks :)" });
-    expect(response.statusCode).toBe(404);
+    expect(response.statusCode).toBe(400);
   });
 });
